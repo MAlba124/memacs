aa2869e3922925c422063c8cf4bebbf2020edfc5
Marcus <marlhan@proton.me>
2024-03-07 21:00:18 +0100

    modeline: custom faces

--- mode_line.el
+++ mode_line.el
@@ -1,14 +1,20 @@
 (require 'all-the-icons)
 (require 'evil)
 
+(defface memacs-modeline-evil-mode-face
+  '((t (:foreground "#f0f1ff" :background "#093060"))) "Evil mode")
+
+(defface memacs-modeline-buffer-face
+  '((t (:foreground "#0a0a0a" :background "#d0d6ff"))) "Buffer")
+
 (setq-default mode-line-format
       '("%e"
         (:eval
          (when (mode-line-window-selected-p)
-           (format "%s%s"
+           (propertize (format "%s%s"
             evil-mode-line-tag
-            "  "
-            )))
+            " "
+            ) 'face 'memacs-modeline-evil-mode-face)))
         memacs-modeline-buffer-name
         (:eval
          (when (mode-line-window-selected-p)
@@ -18,8 +24,8 @@
         ))))
 
 (defvar-local memacs-modeline-buffer-name
-    '(:eval (format "%s %s"
+    '(:eval (propertize (format " %s %s "
                     (all-the-icons-icon-for-file (buffer-name))
-                    (buffer-name))))
+                    (buffer-name)) 'face 'memacs-modeline-buffer-face)))
 
 (put 'memacs-modeline-buffer-name 'risky-local-variable t)
