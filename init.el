7bf795a9acad734bd2f4623c1a1761d50073cb0e
Marcus <marlhan@proton.me>
2024-03-16 09:00:49 +0100

    always ensure packages

--- init.el
+++ init.el
@@ -34,8 +34,7 @@
 
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
-(require 'use-package)
-
+(setq use-package-always-ensure t)
 ;; -----------------------------------------------------------------------------
 ;; -------------------------------- Navigation ---------------------------------
 ;; -----------------------------------------------------------------------------
@@ -43,7 +42,6 @@
 (when memacs-use-evil
     ;; Init evil
     (use-package evil
-    :ensure t
     :init
     (setq evil-want-integration t)
     (setq evil-want-keybinding nil)
@@ -56,7 +54,6 @@
     ;; Install evil-collection for better evil
     (use-package evil-collection
     :after evil
-    :ensure t
     :config
     (evil-collection-init)))
 
@@ -68,7 +65,6 @@
     (unless (package-installed-p 'ivy)
         (package-install 'ivy))
     (use-package ivy
-      :ensure t
       :config
       (setq ivy-use-virtual-buffers t)
       (setq enable-recursive-minibuffers t)
@@ -76,12 +72,10 @@
 
 ;; Install magit
 (when memacs-use-magit
-  (use-package magit
-    :ensure t))
+  (use-package magit))
 
 (when memacs-enable-pomidor
     (use-package pomidor
-      :ensure t
       :bind (("<f12>" . pomidor))
       :config (setq pomidor-sound-tick nil
                       pomidor-sound-tack nil)
@@ -101,26 +95,22 @@
 
 (when memacs-enable-vterm
     (use-package vterm
-    :ensure t
     :config
     (setq vterm-shell "zsh")))
 
 (when memacs-enable-elfeed
   (use-package elfeed
-    :ensure t
     :config
     (setq elfeed-feeds
       '())))
 
 (use-package smartparens
-  :ensure t
   :config
   (smartparens-global-mode 1))
 
 (setq-default fill-column 80)
 
 (use-package company
-  :ensure t
   :init
   (setq company-idle-delay 0.1)
   :hook
@@ -134,7 +124,6 @@
 ;; --------------------------------- Rust
 (when memacs-enable-rust
     (use-package rustic
-      :ensure t
       :bind (:map rustic-mode-map
                   ("C-c C-c t" . rustic-cargo-test)
                   ("C-c C-c b" . rustic-cargo-bench)
@@ -148,39 +137,32 @@
 ;; --------------------------------- HTML
 (when memacs-enable-html
     (use-package web-mode
-      :ensure t
       :init
       (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))))
 
 ;; --------------------------------- Nix
 (when memacs-enable-nix
     (use-package nix-mode
-      :ensure t
       :mode "\\.nix\\'"))
 
 ;; --------------------------------- GLSL
 (when memacs-enable-glsl
   (use-package glsl-mode
-    :ensure t
     :mode ("\\.frag\\'" "\\.vert\\'" "\\.glsl\\'"))
 
   (use-package company-glsl
-    :ensure t
     :mode ("\\.frag\\'" "\\.vert\\'" "\\.glsl\\'")))
 
 (when memacs-enable-lua
-  (use-package lua-mode
-    :ensure t)
+  (use-package lua-mode)
 
   (use-package flymake-lua
-    :ensure t
     :hook
     (lua-mode . flymake-lua-load)))
 
 ;; --------------------------------- Common Lisp
 (when memacs-enable-clisp
   (use-package slime
-    :ensure t
     :init
     (setq slime-lisp-implementations
       '((sbcl ("sbcl" "--dynamic-space-size" "4gb"))))
@@ -190,18 +172,15 @@
 
 ;; --------------------------------- Haskell
 (when memacs-enable-haskell
-  (use-package haskell-mode
-    :ensure t))
+  (use-package haskell-mode))
 
 ;; --------------------------------- Zig
 (when memacs-enable-zig
-  (use-package zig-mode
-    :ensure t))
+  (use-package zig-mode))
 
 ;; --------------------------------- Markdown
 (when memacs-enable-markdown
-  (use-package markdown-mode
-    :ensure t))
+  (use-package markdown-mode))
 
 ;; -----------------------------------------------------------------------------
 ;; -------------------------------- Autoinsert ---------------------------------
@@ -223,24 +202,20 @@
 ;; ---------------------------------- AUCTeX -----------------------------------
 ;; -----------------------------------------------------------------------------
 (use-package auctex
-  :ensure t
   :init
   (setq TeX-PDF-mode t)
   (setq TeX-auto-save t)
   (setq TeX-parse-self t)
   (setq-default TeX-master nil))
 
-(use-package pdf-tools
-  :ensure t)
+(use-package pdf-tools)
 
 ;; -----------------------------------------------------------------------------
 ;; ------------------------------------ UI -------------------------------------
 ;; -----------------------------------------------------------------------------
 (use-package all-the-icons
-  :ensure t
   :if (display-graphic-p))
 (use-package all-the-icons-dired
-  :ensure t
   :hook
   (dired-mode . all-the-icons-dired-mode))
 ;; (add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
