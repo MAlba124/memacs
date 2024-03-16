21cb9690ed6be5f162650b3e0c9dfd6fd3cc5e48
Marcus <marlhan@proton.me>
2024-03-16 09:19:16 +0100

    configuration: minification

--- configuration.el
+++ configuration.el
@@ -15,37 +15,38 @@
 
 ;; Some basic settings you can change
 
-;; Performance
-(setq memacs-native-compilation t)                   ;; Enable native compilation. This required gccemacs
+(setq
+ ;; Performance
+ memacs-native-compilation t                   ;; Enable native compilation. This required gccemacs
 
-;; UI
-(setq memacs-font             "Iosevka Semibold-10") ;; Font to use
-(setq memacs-scroll-margin    5)                     ;; Scroll margin
-(setq memacs-tab-width        4)                     ;; Tab width
-(setq memacs-blink-cursor     0)                     ;; Wheter to enable cursor blinking or not
-(setq memacs-hl-line          t)                     ;; Highlight current line
-(setq memacs-show-linenumbers nil)                   ;; Linenumbers
-(setq memacs-use-mm-modeline  t)                     ;; Wheter to use memacs' modeline
+ ;; UI
+ memacs-font             "Iosevka Semibold-10" ;; Font to use
+ memacs-scroll-margin    5                     ;; Scroll margin
+ memacs-tab-width        4                     ;; Tab width
+ memacs-blink-cursor     0                     ;; Wheter to enable cursor blinking or not
+ memacs-hl-line          t                     ;; Highlight current line
+ memacs-show-linenumbers nil                   ;; Linenumbers
+ memacs-use-mm-modeline  t                     ;; Wheter to use memacs' modeline
 
-;; Packages
-(setq memacs-enable-evil      t)                     ;; Vim keybindings
-(setq memacs-enable-ivy       t)                     ;; Good completion menu
-(setq memacs-enable-magit     t)                     ;; Git UI
-(setq memacs-enable-lsp       t)                     ;; LSP
-(setq memacs-enable-pomidor   nil)                   ;; Pomidor timer
-(setq memacs-enable-elfeed    nil)                   ;; RSS feed reader
-(setq memacs-enable-yas       nil)                   ;; YASnippets
-(setq memacs-enable-vterm     t)                     ;; Terminal emulator
-(setq memacs-vterm-shell      "zsh")                 ;; What shell vterm should use
+ ;; Packages
+ memacs-enable-evil      t                     ;; Vim keybindings
+ memacs-enable-ivy       t                     ;; Good completion menu
+ memacs-enable-magit     t                     ;; Git UI
+ memacs-enable-lsp       t                     ;; LSP
+ memacs-enable-pomidor   nil                   ;; Pomidor timer
+ memacs-enable-elfeed    nil                   ;; RSS feed reader
+ memacs-enable-yas       nil                   ;; YASnippets
+ memacs-enable-vterm     t                     ;; Terminal emulator
+ memacs-vterm-shell      "zsh"                 ;; What shell vterm should use
 
-;; Languages
-(setq memacs-enable-markdown  t)                     ;; Markdown
-(setq memacs-enable-rust      t)                     ;; Rust
-(setq memacs-enable-html      t)                     ;; HTML
-(setq memacs-enable-nix       t)                     ;; Nix
-(setq memacs-enable-sql       nil)                   ;; SQL
-(setq memacs-enable-glsl      nil)                   ;; GLSL
-(setq memacs-enable-lua       nil)                   ;; Lua
-(setq memacs-enable-clisp     nil)                   ;; Common Lisp
-(setq memacs-enable-haskell   nil)                   ;; Haskell
-(setq memacs-enable-zig       nil)                   ;; Zig
+ ;; Languages
+ memacs-enable-markdown  t                     ;; Markdown
+ memacs-enable-rust      t                     ;; Rust
+ memacs-enable-html      t                     ;; HTML
+ memacs-enable-nix       t                     ;; Nix
+ memacs-enable-sql       nil                   ;; SQL
+ memacs-enable-glsl      nil                   ;; GLSL
+ memacs-enable-lua       nil                   ;; Lua
+ memacs-enable-clisp     nil                   ;; Common Lisp
+ memacs-enable-haskell   nil                   ;; Haskell
+ memacs-enable-zig       nil)                  ;; Zig
